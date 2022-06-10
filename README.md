04fc7186ac88d2d03565782fcbd71aa710ec815d
pirpal <peyop78@gmail.com>
2022-06-10 13:19:04 +0200

    Rewrote main.c

--- README.md
+++ README.md
@@ -8,3 +8,16 @@ Basic C functions to manage basic ASCII letters (A-Z and a-z) case
 + `lowercase(char) -> char`
 + `uppercase(char) -> char`
 + `togglecase(char) -> char`
+
+
+## Run example
+
+
+```sh
+$ make
+gcc -g -Wall -Wextra -std=c11 -pedantic main.c ascii_case.c -o PROG
+
+$ ./PROG
+Original string: 'fRODO sAM gANDALF gOLLUM'
+Toggle case: 'Frodo Sam Gandalf Gollum'
+```
--- main.c
+++ main.c
@@ -4,18 +4,15 @@
 
 int main (void) {
 
-  printf("\nUppercase and lowercase ascii letters\n");
-  printf("--------------------------------------\n\n");
-
   const char *s = "fRODO sAM gANDALF gOLLUM";
 
   printf("Original string: '%s'\n", s);
 
-  printf("Toggle case for each character:\n");
+  printf("Toggle case: '");
   for (size_t i = 0; i < strlen(s); ++i) {
     printf("%c", togglecase(s[i]));
   }
 
-  printf("\n");
+  printf("'\n");
   return EXIT_SUCCESS;
 }
